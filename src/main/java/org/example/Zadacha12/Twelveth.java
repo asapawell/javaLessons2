dc580a12ccd606e2d4cf0336c3ffc06acbd7f611
asapawell <asapawell@gmail.com>
2024-01-10 18:46:01 +0300

    12 задача Тест

---
+++ src/main/java/org/example/Zadacha12/Twelveth.java
@@ -0,0 +1,16 @@
+package org.example.Zadacha12;
+//То же что в задаче №11, но удалить все записи с возрастом меньше среднего.
+public class Twelveth {
+    public static void main(String[] args) {
+
+    }
+}
+class User{
+    private final String name;
+    User(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+}
