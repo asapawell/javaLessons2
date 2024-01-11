330158b32739ae1ba6aca3b3de79f312955f24d9
asapawell <asapawell@gmail.com>
2024-01-11 16:02:57 +0300

    13ая задача

---
+++ src/main/java/org/example/Zadacha13/Thirteenth.java
@@ -0,0 +1,22 @@
+package org.example.Zadacha13;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+//Создать карту, где ключом является возраст, а значением - список имен (людей с таким возрастом).
+// Добавить несколько записей. Должны быть люди с одинаковым возрастом. Вывести на экран.
+public class Thirteenth {
+    public static void main(String[] args) {
+        Map<Integer, ArrayList<String>> humans = new HashMap<>();
+        humans.put(22,new ArrayList<String>(Arrays.asList("Pavel","Dima","Max")));
+        humans.put(23,new ArrayList<String>(Arrays.asList("Ramzan","Habib","Antony")));
+        humans.put(24,new ArrayList<String>(Arrays.asList("Ilya","Mitya","Anatoly")));
+        int count = 1;
+        for (Map.Entry<Integer, ArrayList<String>> i:humans.entrySet()){
+            System.out.println(count + "." + "Возраст:" + i.getKey() + " У людей:" + i.getValue());
+            count++;
+        }
+    }
+}
