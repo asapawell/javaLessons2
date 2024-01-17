9c65bc08605f53e69a284f32fa974fd044f3c0ed
asapawell <asapawell@gmail.com>
2024-01-17 20:40:42 +0300

    9ая задача

---
+++ src/main/java/org/example/Zadacha9/Nineth.java
@@ -0,0 +1,28 @@
+package org.example.Zadacha9;
+//Создать список из нескольких объектов User и вывести на экран.
+//Используя stream, увеличить возраст всех пользователей в 2 раза. Исходный список вывести на экран.
+
+import org.example.User.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Nineth {
+    public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
+        users.add(new User(new Name("Pavel"), new Age(27)));
+        users.add(new User(new Name("Anton"), new Age(33)));
+        users.add(new User(new Name("Dima"), new Age(12)));
+        users.add(new User(new Name("Anna"), new Age(25)));
+        users.add(new User(new Name("Michael"), new Age(35)));
+        users.add(new User(new Name("Stepa"), new Age(21)));
+        users.add(new User(new Name("Maria"), new Age(31)));
+        System.out.println("User list: \n" + users);
+        System.out.println("Age raised by 2:");
+        users.stream()
+                .flatMap(user -> Stream.of(
+                        String.format("User: %s, age X2 %d", user.getName().getName(), user.getAge().getAge() * 2))
+                )
+                .forEach(System.out::println);
+    }
+}
