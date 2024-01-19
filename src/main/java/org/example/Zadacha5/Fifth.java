3b0d6c86674bec79b77eb436c1693296b579159b
asapawell <asapawell@gmail.com>
2024-01-19 10:27:29 +0300

    Добавил removeIf через лямбду

--- src/main/java/org/example/Zadacha5/Fifth.java
+++ src/main/java/org/example/Zadacha5/Fifth.java
@@ -1,6 +1,7 @@
 package org.example.Zadacha5;
 
 import org.example.User.*;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -10,26 +11,16 @@ public class Fifth {
     public static void main(String[] args) {
         ArrayList<User> users = new ArrayList<>();
         Collections.addAll(users,
-                new User("RBeda",39),
-                new User("SevAlieva",38),
-                new User("MRyabov",55),
-                new User("GAmidi",32));
+                new User("RBeda", 39),
+                new User("SevAlieva", 38),
+                new User("MRyabov", 55),
+                new User("GAmidi", 32));
 
         System.out.println("User list:\n" + users);
         System.out.println();
-        System.out.println("Обновленный список пользователей, чей возраст менее 40 лет:");
-        //создаем новый список "К удалению". в него добавляем пользователей, чей возраст больше 40 лет
-        ArrayList<User> toRemove = new ArrayList<>();
-        for (User u : users) {
-            if (u.getAge() > 40) {
-                toRemove.add(u);
-            }
-        }
-        //на вход методу передаем список к удалению, который удалит пользователей из изначального списка Users
-        users.removeAll(toRemove);
-        for (User u : users) {
-            System.out.printf("%s, Возраст: %d\n", u.getName(), u.getAge());
-        }
+        //через лямбду
+        users.removeIf(user -> user.getAge() < 40);
+        System.out.println("User list, where users 're older 40:\n" + users);
     }
 }
 
