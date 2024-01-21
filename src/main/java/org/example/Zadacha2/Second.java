eb865140c8cb1bad953a1ba7980d9053e3330da6
asapawell <asapawell@gmail.com>
2024-01-21 17:42:29 +0300

    Правки

--- src/main/java/org/example/Zadacha2/Second.java
+++ src/main/java/org/example/Zadacha2/Second.java
@@ -1,6 +1,7 @@
 package org.example.Zadacha2;
 
 //Создать список из пяти элементов и вывести на экран. Добавить один элемент, удалить другой элемент и вывести на экран.
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -9,28 +10,28 @@ import java.util.Random;
 public class Second {
     public static void main(String[] args) {
         System.out.println("User list:");
-        ArrayList<Integer> nums3 = printList(new ArrayList<>());
+        ArrayList<Integer> nums3 = printList();
         System.out.println("List after adding 777:");
         nums3.add(777);
-        System.out.println(nums3.toString());
+        System.out.println(nums3);
         System.out.println("List after deleting fourth element:");
         nums3.remove(3);
-        System.out.println(nums3.toString());
+        System.out.println(nums3);
         System.out.println();
         //второй способ
         List<Integer> listOfNums = new ArrayList<>();
-        Collections.addAll(listOfNums,3,32,33,21,5);
+        Collections.addAll(listOfNums, 3, 32, 33, 21, 5);
         System.out.println(listOfNums);
     }
 
-    public static ArrayList<Integer> printList(ArrayList<Integer> list) {
+    public static ArrayList<Integer> printList() {
         Random rnd = new Random();
         ArrayList<Integer> nums2 = new ArrayList<>();
         //цикл for до 5 тк капасити не задает длину списка, а задает максимальную вместимость. for each не знаю как использовать
         for (int i = 0; i < 5; i++) {
             nums2.add(rnd.nextInt(100));
         }
-        System.out.println(nums2.toString());
+        System.out.println(nums2);
         return nums2;
     }
 }
