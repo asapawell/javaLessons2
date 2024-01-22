2517ab32e1955cf810e619f263866f0f059e7503
asapawell <asapawell@gmail.com>
2024-01-22 16:20:38 +0300

    Добавил обработку ошибок и условие if

--- src/main/java/org/example/Zadacha11/Eleventh.java
+++ src/main/java/org/example/Zadacha11/Eleventh.java
@@ -4,11 +4,12 @@ package org.example.Zadacha11;
 import org.example.User.*;
 
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Eleventh {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UserException {
         Map<User, Integer> users = new HashMap<>();
         users.put(new User("Pavel"), 22);
         users.put(new User("Tom"), 29);
@@ -16,18 +17,31 @@ public class Eleventh {
         users.put(new User("Dima"), 22);
         users.put(new User("Anton"), 40);
         users.put(new User("Max"), 28);
-        System.out.println("Карта до удаления:");
+        System.out.println("Map before deleting:");
         for (Map.Entry<User, Integer> entry : users.entrySet()) {
             System.out.println(entry.getKey().getName() + ":" + entry.getValue());
         }
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите возраст к удалению:");
-        int age = scanner.nextInt();
-        users.entrySet().removeIf(entry -> entry.getValue() == age);
-        System.out.println("Карта после удаления");
-        for (Map.Entry<User, Integer> entry : users.entrySet()) {
-            System.out.println(entry.getKey().getName() + ":" + entry.getValue());
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            int age;
+            try {
+                System.out.println("Enter age to delete:");
+                age = scanner.nextInt();
+                if (users.containsValue(age)) {
+                    users.entrySet().removeIf(entry -> entry.getValue() == age);
+                } else {
+                    System.out.println("Enter the age, that contains in map!");
+                    continue;
+                }
+            } catch (InputMismatchException exception) {
+                System.out.println("Enter a number!");
+                continue;
+            }
+            System.out.println("Map after deleting");
+            for (Map.Entry<User, Integer> entry : users.entrySet()) {
+                System.out.println(entry.getKey().getName() + ":" + entry.getValue());
+            }
+            break;
         }
     }
 }
-
