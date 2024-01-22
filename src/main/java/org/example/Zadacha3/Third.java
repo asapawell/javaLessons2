099a4e98741d1b4da5ccd50856f52d142cb02632
asapawell <asapawell@gmail.com>
2024-01-22 15:32:51 +0300

    Добавил обработку ошибок

--- src/main/java/org/example/Zadacha3/Third.java
+++ src/main/java/org/example/Zadacha3/Third.java
@@ -3,24 +3,40 @@ package org.example.Zadacha3;
 // Значение N программа должна запросить с клавиатуры, данные пользователей User программа также должна запрашивать с клавиатуры. Вывести результат на экран.
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
 import org.example.User.*;
 
 public class Third {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите размерность списка:");
-        int sizeOfList = scanner.nextInt();
-        ArrayList<User> users = new ArrayList<>();
-        for (int i = 0; i < sizeOfList; i++) {
-            System.out.println("Введите имя:");
-            String name = scanner.next();
-            System.out.println("Введите возраст:");
-            int age = scanner.nextInt();
-            User user = new User(name, age);
-            users.add(user);
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            int sizeOfList;
+            String name;
+            int age;
+            try {
+                System.out.println("Enter size of list:");
+                sizeOfList = scanner.nextInt();
+                ArrayList<User> users = new ArrayList<>();
+                for (int i = 0; i < sizeOfList; i++) {
+                    System.out.println("Enter name:");
+                    name = scanner.next();
+                    System.out.println("Enter age:");
+                    age = scanner.nextInt();
+                    User user = new User(name, age);
+                    users.add(user);
+                }
+                System.out.println("User list:\n" + users);
+            } catch (UserException e) {
+                System.out.println("Error:" + e.getMessage());
+                continue;
+            }
+            catch (InputMismatchException e){
+                System.out.println("Enter a number!");
+                continue;
+            }
+            break;
         }
-        System.out.println("User list:\n" + users);
     }
 }
-
