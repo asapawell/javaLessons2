26a4b1ecb923257e381c729f1f62b24609ade306
asapawell <asapawell@gmail.com>
2024-01-22 16:26:19 +0300

    Пробросил необрабатываемое исключение UserException

--- src/main/java/org/example/Zadacha10/Tenth.java
+++ src/main/java/org/example/Zadacha10/Tenth.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 //Создать карту в которой ключом должно являться имя, а значением возраст пользователя. Добавить несколько значений, вывести на экран.
 public class Tenth {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UserException {
         Map<User, Integer> people = new HashMap<>();
         people.put(new User("Alex"), 28);
         people.put(new User("Tom"), 12);
